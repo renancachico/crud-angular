3fadc2404cac3003e03ef1c530559deb80cd69c4
rsantos68 <rsantos68@dxc.com>
2022-09-15 15:39:29 -0300

    Atualizar Curso: Salvando o Formulário

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -16,9 +16,9 @@ export class CoursesService {
   list() {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
-      first(),
-      delay(3000),
-      tap(courses => console.log(courses))
+      first()//,
+      //delay(3000),
+      //tap(courses => console.log(courses))
     );
   }
 
@@ -27,6 +27,21 @@ export class CoursesService {
   }
 
   save(record: Partial<Course>){
+    console.log(record);
+    if(record._id){
+      console.log('update');
+      return this.update(record);
+    }
+
+    console.log('create');
+    return this.create(record);
+  }
+
+  private create(record: Partial<Course>){
     return this.httpClient.post<Course[]>(this.API, record).pipe(first());
+  }
+
+  private update(record: Partial<Course>){
+    return this.httpClient.put<Course[]>(`${this.API}/${record._id}`, record).pipe(first());
   }
 }
